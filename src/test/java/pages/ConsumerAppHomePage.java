9721866dc57732b2b4c5ed545a45e033be754612
evgeniia.litvina <rusakova.evg@gmail.com>
2023-11-02 13:33:50 +0400

    Added pagination methods for Directory search results, optimized imports where needed

--- src/test/java/pages/ConsumerAppHomePage.java
+++ src/test/java/pages/ConsumerAppHomePage.java
@@ -2,9 +2,9 @@ package pages;
 
 import com.codeborne.selenide.SelenideElement;
 
+import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selectors.*;
 
 public class ConsumerAppHomePage {
 
